f4d7535ea1dcacfefe6b3446eed66d0339554a73
tomoyo519 <tomoyo519@naver.com>
2023-10-26 16:25:44 +0900

    apple coding chapter4 homework

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-          child: Container( width: 50, height: 50, color: Colors.blue)
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white ,
+          actions: [IconButton(icon: Icon(Icons.search) , onPressed:(){}, color: Colors.black,)
+          ,IconButton(icon: Icon(Icons.menu) , onPressed:(){}, color: Colors.black,)
+          ,IconButton(icon: Icon(Icons.doorbell) , onPressed:(){} , color: Colors.black,)],
+          title:Row(
+            children: [
+              Text('금호동3가', style: TextStyle(color:(Colors.black), fontWeight: FontWeight.w700 )),
+              IconButton(onPressed: (){}, icon: Icon(Icons.expand_more), color: Colors.black,)
+            ],
+          ),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          margin: EdgeInsets.all(10),
+          child: Row(
+
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+
+                Image.asset( "assets/wow.png", height: 120, width: 120,),
+                SizedBox(
+                  width: 250, height :250,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children: [
+
+                        Text("캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)", style: TextStyle(fontSize: 20),),
+                        Text("성동구 행당동 끌올 10분전 ",),
+                        Text("210,000원", style: TextStyle(fontWeight:FontWeight.w800)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children:[
+                            IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border)),
+                            Text("4")
+                          ]
+
+                        )
+
+                      ],
+                    ),
+                  ),
+                )
+              ],
+             ),
+        ),
+
+
+        bottomNavigationBar: BottomAppBar(child:SizedBox(
+          height: 70,
+          child: Row(
+
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            Icon(Icons.phone),
+            Icon(Icons.message),
+            Icon(Icons.contact_page)
+          ],
+    ),
+        ),
       )
+    )
     );
   }
 }
