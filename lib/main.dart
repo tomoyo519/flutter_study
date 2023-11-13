12d8f2526bb9290719087700ea44f6267e693022
tomoyo519 <tomoyo519@naver.com>
2023-11-13 20:58:32 +0900

    apple coding 9강 숙제 완료

--- lib/main.dart
+++ lib/main.dart
@@ -2,77 +2,78 @@ import 'package:flutter/material.dart';
 
 void main() {
   //앱 시작해주세요.
-  runApp(const MyApp());
+  runApp(
+      MaterialApp(
+        home: MyApp()
+      )
+    );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+           showDialog(context:context, builder:(context){
+             return Dialog(
+
+                 child:
+             Container(
+               height:250,
+               width: 250,
+               padding: EdgeInsets.all(10),
+               child: Column(
+                   mainAxisAlignment: MainAxisAlignment.center,
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                 children:[
+                   Text("Contact",style: TextStyle(fontWeight: FontWeight.w800, fontSize:20) ,),
+                   TextField(),
+                   Row(
+
+                       mainAxisAlignment: MainAxisAlignment.end,
+                     children:[
+                       ElevatedButton(onPressed: (){
+                         Navigator.pop(context);
+                       }, child: Text("Cancel")),
+                       ElevatedButton(onPressed: (){}, child: Text("OK"))
+
+                     ]
+
+
+                   )
+
+                 ]
+
+               ),
+             )
+             );
+           });
+          },
+        ),
         appBar: AppBar(
           backgroundColor: Colors.white ,
           actions: [IconButton(icon: Icon(Icons.search) , onPressed:(){}, color: Colors.black,)
           ,IconButton(icon: Icon(Icons.menu) , onPressed:(){}, color: Colors.black,)
           ,IconButton(icon: Icon(Icons.doorbell) , onPressed:(){} , color: Colors.black,)],
           title:Row(
-            children: [
-              Text('금호동3가', style: TextStyle(color:(Colors.black), fontWeight: FontWeight.w700 )),
-              IconButton(onPressed: (){}, icon: Icon(Icons.expand_more), color: Colors.black,)
-            ],
+
           ),
         ),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.all(10),
-          child: Row(
+        body: Text('')
 
-            crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Flexible(child: Image.asset( "assets/wow.png", height: 120, width: 120 ),flex:1),
-                Flexible(child: Container(
-                  padding: EdgeInsets.all(10),
-                  width: 250, height :250,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)", style: TextStyle(fontSize: 20),),
-                      Text("성동구 행당동 끌올 10분전 ",),
-                      Text("210,000원", style: TextStyle(fontWeight:FontWeight.w800)),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children:[
-                          IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border)),
-                          Text("4")
-                        ]
-                      )
-                    ],
-                  ),
-                ), flex:4)
-              ],
-             ),
-        ),
+      );
 
 
-        bottomNavigationBar: BottomAppBar(child:SizedBox(
-          height: 70,
-          child: Row(
-
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-
-            Icon(Icons.phone),
-            Icon(Icons.message),
-            Icon(Icons.contact_page)
-          ],
-    ),
-        ),
-      )
-    )
-    );
   }
 }
+
